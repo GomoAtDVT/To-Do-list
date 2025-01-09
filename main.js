c362e30aec97c8cf0bb484388b148d1e34a0b8f0
Gomolemo Motlhamme <gmotlhamme@jhb.dvt.co.za>
2025-01-09 15:06:27 +0200

    adding the search functionality

--- main.js
+++ main.js
@@ -25,13 +25,24 @@ submitbtn.addEventListener('click', function() {
    }else {
     //create a new list item to store values from the user
     const li = document.createElement('li');
+    // let counter = 0;
    li.appendChild(document.createTextNode(inputTask.value));
     document.querySelector('ul').appendChild(li);
     li.innerHTML = `${inputTask.value} <div id="icons"><abbr title="Edit"><i class="bi bi-pencil-square"></i></abbr><abbr title="Delete"><i class="bi bi-trash3"></i></abbr></div>`;
+    // while (li) {
+    //     li++ ;
+    //     console.log(li);
+        
     inputTask.value = '';
+
    }
 });
  
+// const taskText = document.createElement('span');
+// taskText.contentEditable = true;
+// taskText.textContent = task.text;
+// taskText.style
+
 //function to remove a task
 function removeTask(e) {
     if(e.target.classList.contains('bi-trash3')) {
@@ -52,3 +63,22 @@ function editTask(e) {
     }
 }
 document.querySelector('ul').addEventListener('click', editTask);
+
+// function to search for a task
+function searchTask() {
+    const searchInput = document.querySelector('#search');
+    const filter = searchInput.value.toLowerCase();
+    const tasks = document.querySelectorAll('ul li');
+
+    tasks.forEach(task => {
+        const taskText = task.firstChild.textContent.toLowerCase();
+        if (taskText.includes(filter)) {
+            task.style.display = '';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+}
+
+// event listener for the search input
+document.querySelector('#search').addEventListener('keyup', searchTask);
